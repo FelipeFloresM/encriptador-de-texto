9351c72ee820ddc63bfaabc114a118ef87162801
FelipeFloresM <felipe.floresm1996@gmail.com>
2024-08-27 22:10:37 -0400

    Subiendo proyecto

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function encriptar(){
     let textoCifrado = texto
                         .replace(/a/gi, "ara")
                         .replace(/e/gi, "estyl")
-                        .replace(/i/gi, "iman")
+                        .replace(/i/gi, "img")
                         .replace(/o/gi, "oso")
                         .replace(/u/gi, "uwu");
                         
@@ -21,5 +21,32 @@ function encriptar(){
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
         alert("Favor ingresar un texto");
+    }
+}
+
+
+function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let persona = document.getElementById("persona");
+
+    let textoCifrado = texto
+        .replace(/ara/gi, "a")
+        .replace(/estyl/gi, "e")
+        .replace(/img/gi, "i")
+        .replace(/oso/gi, "o")
+        .replace(/uwu/gi, "u");
+
+        if (texto.length !=0){
+            document.getElementById("texto").value= textoCifrado;
+            tituloMensaje.textContent= "Texto desencriptado con éxito";
+            parrafo.textContent = "";
+            persona.src = "./img/desencriptado.png";
+} else {
+    persona.src = "./img/Muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    alert("Favor ingresar un texto");
     }
 }
